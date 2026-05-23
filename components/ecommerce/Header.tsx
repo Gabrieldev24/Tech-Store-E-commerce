a9c7a66b4108724ec9afa33a84f062430ffc3646
Gabrieldev24 <N00234628@upn.pe>
2026-05-23 18:07:42 -0500

    arregle

--- components/ecommerce/Header.tsx
+++ components/ecommerce/Header.tsx
@@ -56,7 +56,7 @@ export function Header() {
           {/* Logo */}
           <Link href={user?.role === 'admin' ? '/admin/dashboard' : '/'} className="flex items-center gap-2">
             <Image 
-              src="/logo.png" 
+              src="/logo-blanco.png" 
               alt="TechStore" 
               width={32} 
               height={32}
