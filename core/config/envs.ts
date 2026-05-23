7bb8acf8725f54fd63cbf995a46f1bd07455e10c
Gabrieldev24 <N00234628@upn.pe>
2026-05-23 16:38:16 -0500

    envs port

--- core/config/envs.ts
+++ core/config/envs.ts
@@ -4,7 +4,7 @@ import * as env from 'env-var';
 
 export const envs = {
      
-        PORT : env.get('PORT').required().asPortNumber(),
+        PORT : env.get('PORT').required().default(3000).asPortNumber(),
         PUBLIC_PATH: env.get('PUBLIC_PATH').default('public').asString(),
         POSTGRES_URL: env.get('POSTGRES_URL').asUrlString(),
         POSTGRES_USER: env.get('POSTGRES_USER').asString(),
